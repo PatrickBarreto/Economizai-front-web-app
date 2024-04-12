89bcc8ff65cbfbb930d7408e2ef8bc87c7fbad71
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-04-12 16:09:22 -0300

    Add ladingPage route and create accout route

--- src/routes/RouteSwitcher.tsx
+++ src/routes/RouteSwitcher.tsx
@@ -3,13 +3,17 @@ import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
 import Products from '../pages/Products/Products';
 import TestComponente from '../pages/testComponent';
+import { CreareAcccount } from '../pages/CreateAccount/CreateAccount';
+import { LandingPage } from '../pages/LandingPage/LadingPage';
 
 const RouteSwitcher:React.FC = () => {
     
     return (
         <Router>
             <Routes>
+                <Route  path="/" element={<LandingPage/> }/>
                 <Route  path="/login" element={<Login/> }/>
+                <Route  path="/createAccount" element={<CreareAcccount/>}/>
                 <Route  path="/home" element={<Home/>}/>
                 <Route  path="/products" element={<Products/>}/>
                 <Route  path="/testeComponent" element={<TestComponente/>}/>
