fe351221a39478ff4a67d9b3fc36eb64e28b58e4
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-04-12 16:23:39 -0300

    change import testComponent

--- src/routes/RouteSwitcher.tsx
+++ src/routes/RouteSwitcher.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
 import Products from '../pages/Products/Products';
-import TestComponente from '../pages/testComponent';
+import { TestComponent } from '../components/testComponent.tsx';
 import { CreareAcccount } from '../pages/CreateAccount/CreateAccount';
 import { LandingPage } from '../pages/LandingPage/LadingPage';
 
@@ -16,7 +16,7 @@ const RouteSwitcher:React.FC = () => {
                 <Route  path="/createAccount" element={<CreareAcccount/>}/>
                 <Route  path="/home" element={<Home/>}/>
                 <Route  path="/products" element={<Products/>}/>
-                <Route  path="/testeComponent" element={<TestComponente/>}/>
+                <Route  path="/testComponent" element={<TestComponent/>}/>
             </Routes>
         </Router>
     );
