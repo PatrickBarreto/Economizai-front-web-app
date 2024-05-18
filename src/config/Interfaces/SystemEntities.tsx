9ce213fcccc6d4fd6d7f11499bc1a98f88b061fc
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-05-18 18:19:10 -0300

    Create new interface system entities

--- src/config/Interfaces/SystemEntities.tsx
+++ src/config/Interfaces/SystemEntities.tsx
@@ -11,13 +11,36 @@ export interface Product {
     unit_mensure:string,
 }
 
-export interface Categories {
+export interface Brand {
+    id?:number|string,
+    name:string,
+    type?:string
+}
 
+export interface Categories {
+    id?:number|string,
+    name:string
+}
+
+export interface CategoriesProducts {
+    id?:number|string,
+    categories_id:string,
+    products_id:string
+}
+
+export interface CategoriesBrands {
+    id?:number|string,
+    categories_id:string,
+    brands_id:string
 }
 
 
+
+
 export interface ShoppingList {
-    
+    id?:number|string,
+    name : string,
+    type : string
 }
 
 
