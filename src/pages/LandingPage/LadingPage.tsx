d2fe3d8f4a8465d0bba69cba0e8641ffa9a9cdee
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-05-18 18:39:38 -0300

    Add lading page to new accounts

--- src/pages/LandingPage/LadingPage.tsx
+++ src/pages/LandingPage/LadingPage.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Header } from '../../components/Structure/Structure';
+import { Footer, Header, Main } from '../../components/Structure/Structure';
 import { HeaderTemplate } from '../../templates/Header/Header';
+import { PublicFooter } from '../../templates/Footer/PublicFooter';
+import { Title } from '../../components/SubComponents/Title';
+import { useNavigate } from 'react-router-dom';
+import { CtaButton } from '../../templates/Button/CtaButton';
 
 export const LandingPage:React.FC<any> = () => {
+
+    const navigate = useNavigate();
+
 
     return (
         <>
             <Header>
                 <HeaderTemplate />
             </Header>
+            <Main >
+                <Title content="Economizaí" />
+                <CtaButton action={()=>{navigate('/login')}} content="Login"/>
+                <CtaButton action={()=>{navigate('/createAccount')}} content="Criar Conta"/>
+            </Main>
+            <Footer>
+                <PublicFooter />
+            </Footer>
         </>
     );
 }
