f4b50408c19a2e668739f705d52b00b3dc1c506a
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-05-18 18:07:11 -0300

    add new routes

--- src/routes/RouteSwitcher.tsx
+++ src/routes/RouteSwitcher.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import Home from '../pages/Home/Home';
-import Products from '../pages/Products/Products';
-import { TestComponent } from '../components/testComponent.tsx';
+
+import Login              from '../pages/Login/Login';
+import Home               from '../pages/Home/Home';
+import Products           from '../pages/Products/Products';
+import Brands             from '../pages/Brands/Brands.tsx';
 import { CreareAcccount } from '../pages/CreateAccount/CreateAccount';
-import { LandingPage } from '../pages/LandingPage/LadingPage';
+import { LandingPage }    from '../pages/LandingPage/LadingPage';
+import Categories         from '../pages/Categories/Categories.tsx';
+import ShoppingLists      from '../pages/ShoppingLists/ShoppingLists.tsx';
+import { TestComponent }  from '../components/testComponent.tsx';
 
 const RouteSwitcher:React.FC = () => {
     
@@ -16,6 +20,9 @@ const RouteSwitcher:React.FC = () => {
                 <Route  path="/createAccount" element={<CreareAcccount/>}/>
                 <Route  path="/home" element={<Home/>}/>
                 <Route  path="/products" element={<Products/>}/>
+                <Route  path="/brands" element={<Brands/>}/>
+                <Route  path="/categories" element={<Categories/>}/>
+                <Route  path="/shopping-list" element={<ShoppingLists/>}/>
                 <Route  path="/testComponent" element={<TestComponent/>}/>
             </Routes>
         </Router>
