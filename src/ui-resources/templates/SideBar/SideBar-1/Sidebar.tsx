ac6e4552d59c237478a9b5a1f9f340e1b64344b3
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-03-18 22:21:38 -0300

    ECON-FRON-7 Improve sidebar

--- src/ui-resources/templates/SideBar/SideBar-1/Sidebar.tsx
+++ src/ui-resources/templates/SideBar/SideBar-1/Sidebar.tsx
@@ -1,19 +1,19 @@
 import menu from './SidebarLinks.tsx'
 import '../../styles/SideBar-1.css'
 import { CiSettings } from "react-icons/ci";
+import { useLocation } from 'react-router-dom';
 
 
 export const SideBar = () => {
     return( 
       <div id="sidebar-1-div">
         <div id="sidebar-1-div-list-options">
-          {
-            menu.map((m, index) => (
-              <p key={index}>
-                <a href={m.link}>{m.label}</a>
-              </p>
-            ))
-          }
+          {menu.map((m, index) => {
+            const className = useLocation().pathname == m.link ? 'selected' : 'not-selected';
+            return <p key={index}>
+                <a className={className} href={m.link}>{m.label}</a>
+            </p>
+          })}
         </div>
         <div id="sidebar-1-setting">
           <CiSettings size={30} color="gray"/>
