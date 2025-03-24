09a4586cf7ca718639f553e879ec06385a155b25
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-03-24 10:54:47 -0300

    ECON-FRON-7 Fiz main component template

--- src/ui-resources/templates/Structure/Main/Main-1/Main-1.tsx
+++ src/ui-resources/templates/Structure/Main/Main-1/Main-1.tsx
@@ -1,11 +1,18 @@
+import React from "react"
 import { SideBar } from "../../../SideBar/SideBar-1/Sidebar"
-import '../../../styles/Main-1.css'
 import { Main as MainComponent } from "../../../../components/Structure/Structure"
+import { Main as MainInterface} from "../../../../../config/Interfaces/Structure"
+import './Main-1.css'
 
-const Main = () => {
+const Main:React.FC<MainInterface> = ({children}) => {
   return (
     <MainComponent>
       <SideBar/>
+      <section id="main-1-content-section">
+        <div id="main-1-content-div">
+          { children }
+        </div>
+      </section>
     </MainComponent>
   )
 }
