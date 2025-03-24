598e7cad970ba0e3aae6628122141de1db31f846
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-03-24 10:40:03 -0300

    ECON-FRON-7 Turn search component in Template

---
+++ src/ui-resources/templates/Search/Search-1.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { RiSearchLine } from "react-icons/ri";
+import Form from "../../../ui-resources/components/Resources/Form/Form";
+import Input from "../../../ui-resources/components/Resources/Form/Input/Input";
+import { Search } from "../../../config/Interfaces/Search";
+import { z } from "zod";
+import '../styles/Search.css'
+
+export const SearchInput:React.FC<Search> = ({submitCallback}) => {
+
+    const zodObjectSchema = z.object({
+        searchBrands: z.string()
+    });
+    
+
+    return (
+      <>
+        <div className="divSearch">
+          <div className="searchIcon">
+              <RiSearchLine/>
+          </div>
+          <Form className={"seachForm"} submitCallback={ submitCallback } zodObject={ zodObjectSchema }>
+              <Input 
+                  name={"searchBrands"}
+                  className={"seachFormInput"}
+                  type={"search"}
+                  placeholder={"Digite sua busca"}
+              />
+          </Form>
+        </div>
+      </>
+    )
+
+}
