fd588bba0e9074cf01e388e0fc70a140aef594c8
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-03-24 10:54:13 -0300

    ECON-FRON-7 Move modal css to modeo folder

--- src/ui-resources/templates/Modal/Modal.tsx
+++ src/ui-resources/templates/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Modal as ModalComponent, ModalInterface } from "../../components/Modal/Modal"
+import './Modal.css'
 
 
 export const Modal:React.FC<ModalInterface> = ({ children }) => {
