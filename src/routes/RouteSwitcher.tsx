caa53e4d6e9beceac2601c31323c42a101d75f6f
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-05-26 19:44:04 -0300

    ECO-F-26 create new routes to category forms

--- src/routes/RouteSwitcher.tsx
+++ src/routes/RouteSwitcher.tsx
@@ -10,6 +10,7 @@ import { LandingPage }    from '../pages/LandingPage/LadingPage';
 import Categories         from '../pages/Categories/Categories.tsx';
 import ShoppingLists      from '../pages/ShoppingLists/ShoppingLists.tsx';
 import { TemplateFactory }  from '../ui-resources/templates/TemplateFactory.tsx';
+import { CategoriesCreateForm, CategoriesEditForm } from '../pages/Categories/CategoriesForms.tsx';
 
 const RouteSwitcher:React.FC = () => {
     
@@ -26,7 +27,11 @@ const RouteSwitcher:React.FC = () => {
                 <Route  path="/products/:id/edit" element={<ProductEditForm/>}/>
                 
                 <Route  path="/brands" element={<Brands/>}/>
+
                 <Route  path="/categories" element={<Categories/>}/>
+                <Route  path="/categories/create" element={<CategoriesCreateForm/>}/>
+                <Route  path="/categories/:id/edit" element={<CategoriesEditForm/>}/>
+                
                 <Route  path="/shopping-list" element={<ShoppingLists/>}/>
                 <Route  path="/templateFactory" element={<TemplateFactory/>}/>
             </Routes>
